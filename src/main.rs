fbf4792caf20d1fcf1acdf4b23653e71dd06f885
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-07-14 17:25:12 -0300

    Implementing MResult and tests

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod linked_list;
 mod block;
 mod mresult;
 
+use mresult::MResult;
+
 fn main() {
     println!("Hello, world!");
+    let result: MResult<i32, &str> = MResult::ok(42);
+        // this function should raise a panic! due to result being unwraped is not Err
+    result.unwrap_err();
 }
