bed6f648427c1993a891ad155e15b51beccade4f
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-07-14 19:47:25 -0300

    Implementing LinkedList methods and using it for block.rs with unit tests

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
 
-use std::collections::LinkedList as List;
+// for some reason, importing the module here is not working, so I 
+// needed to use crate::linked_list
+// mod linked_list;
+
+use crate::linked_list::{LinkedList as List, Node};
 
 struct BlockChain {
     blocks: List<Block>
@@ -9,28 +13,22 @@ struct BlockChain {
 struct Block {
     hash: String,
     id: u128,
-    transactions: List<Transaction>,
+    transactions: List<Transaction>
 }
 
 struct Transaction {
     inputs: List<TxIn>,
     outputs: List<TxOut>,
-    txid: String,
+    txid: String
 }
 
 struct TxIn {
     prev_txid: String,
     out: usize,
-    signature: String, // to spend the output
+    signature: String
 }
 
 struct TxOut {
-    public_address: String,
-    satoshis: u64, 
-    // 1 btc = 10^8 satoshis, in total 10^8 * 21 * 10^6 = 2.1 * 10^15
-    // maximum value of u64 is greater than 10^19
-    // so u64 is enough to store all valid satoshis
+    pub_address: String,
+    sats: u64,
 }
-
-// Try to include bitcoin related functionalities like serialization, computing addresses etc.,
-// You can add your own methods for different types and associated unit tests
--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,12 +1,79 @@
 #![allow(unused)]
 
-/* This module will be taught in the class */
+use std::thread::current;
 
-struct LinkedList<T> {
-    head: Option<Node<T>>,
+#[derive(Clone)]
+pub struct LinkedList<T> {
+    pub head: Option<Box<Node<T>>>,
 }
 
-struct Node<T> {
-    val: T,
-    next: Option<Box<Node<T>>>,
+#[derive(Clone)]
+pub struct Node<T> {
+    pub val: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> LinkedList<T>{
+    pub fn new() -> Self {
+        LinkedList { head: Option::None }
+    }
+
+    pub fn push(&mut self, val: T) {
+        let new_node = Box::new(
+            Node {
+                val,
+                next: Option::None,
+            }
+        );
+
+        if self.head.is_none() {
+            self.head = Some(new_node);
+            return;
+        }
+
+        let mut current_node = self.head.as_mut().unwrap();
+
+        while !current_node.next.is_none() {
+            current_node = current_node.next.as_mut().unwrap();
+        }
+
+        current_node.next = Some(new_node)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_linked_list_is_empty() {
+        let list: LinkedList<i32> = LinkedList::new();
+        assert!(list.head.is_none());
+    }
+
+    #[test]
+    fn test_push_single_element() {
+        let mut list = LinkedList::new();
+        list.push(42);
+        assert!(list.head.is_some());
+        assert_eq!(list.clone().head.unwrap().val, 42);
+        assert!(list.clone().head.unwrap().next.is_none()); // No next node
+    }
+
+    #[test]
+    fn test_push_multiple_elements() {
+        let mut list = LinkedList::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Verify the entire structure of the list
+        let mut current_node = list.head.as_ref();
+        assert_eq!(current_node.unwrap().val, 1); // First node
+        current_node = current_node.unwrap().next.as_ref();
+        assert_eq!(current_node.unwrap().val, 2); // Second node
+        current_node = current_node.unwrap().next.as_ref();
+        assert_eq!(current_node.unwrap().val, 3); // Third node
+        assert!(current_node.unwrap().next.is_none()); // No fourth node
+    }
 }
